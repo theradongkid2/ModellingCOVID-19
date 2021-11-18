d1f36171a6994dd59a2f6fa2884875e3c94cf530
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-11-18 20:54:48 +1100

    works for stage 1

--- index.cpp
+++ index.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int ppl, c0, r0, contagiousDays, recoveryDay;
+float ppl, c0, r0;
+int contagiousDays, recoveryDay;
+
+float newCases[1000000];
 
 int main() {
     freopen("in.txt", "r", stdin);
@@ -10,27 +14,71 @@ int main() {
     cin >> ppl >> c0 >> r0 >> contagiousDays >> recoveryDay;
 
     int day = 1;
-    int pplNotHadCovid = ppl-c0;
-    int totalCases = c0;
-    int prevCatch = c0;
-    int fooCatch = c0;
-    int totalRecovered = 0;
+    float pplNotHadCovid = ppl-c0;
+    float totalCases = c0;
+    float prevCatch = c0;
+    float fooCatch = c0;
+    float totalRecovered = 0;
+    int actrecoveryDay = contagiousDays-1;
+    int recoveryDayCount = 1;
+    int contagiousDayCount = 1;
+    cout << "----------------------------------------------------------------------------------------------------------------" << endl;
 
-    cout << "Day: | Active Total COVID Cases: | Total Recovered: | Population never had COVID: \n";
+    cout << "   ___ _____   _____ ___      _ ___   __  __        _     _ _ _             ___                             \n"
+            "  / __/ _ \\ \\ / |_ _|   \\ ___/ / _ \\ |  \\/  |___ __| |___| | (_)_ _  __ _  | _ \\_ _ ___ __ _ _ _ __ _ _ __  \n"
+            " | (_| (_) \\ V / | || |) |___| \\_, / | |\\/| / _ / _` / -_| | | | ' \\/ _` | |  _| '_/ _ / _` | '_/ _` | '  \\ \n"
+            "  \\___\\___/ \\_/ |___|___/    |_|/_/  |_|  |_\\___\\__,_\\___|_|_|_|_||_\\__, | |_| |_| \\___\\__, |_| \\__,_|_|_|_|\n"
+            "                                                                    |___/              |___/                \n";
 
-    while (pplNotHadCovid > 0) {
-        if ((day-1) % recoveryDay == 0 && day != 1) {
+    cout << "  By Joshua Koh" << endl;
+    cout << "----------------------------------------------------------------------------------------------------------------" << endl;
+
+    cout << endl;
+
+    cout << "Input: " << endl;
+
+    cout << endl;
+
+    cout << "Population: " << ppl << endl;
+    cout << "Population Infected on Day 1: " << c0 << endl;
+    cout << "r0 Number: " << r0 << endl;
+    cout << "Day in Cycle for Spreading Virus :" << contagiousDays << endl;
+    cout << "Day in Cycle for Recovery: " << recoveryDay << endl;
+
+    cout << endl;
+    cout << endl;
+
+    cout << "Results (Modelled Table):"<< endl;
+
+    cout << "----------------------------------------------------------------------------------------------------------------\n";
+    cout << setw(5) << "Day" << setw(20) << "Active Infections:" << setw(20) << "Total Recovered:" << setw(35) << "Total Recovered + Infections:" << setw(30) << "Never Infected Population:" << endl;
+    cout << "----------------------------------------------------------------------------------------------------------------\n";
+
+    while (pplNotHadCovid > 0 || totalCases > 0) {
+        if ((day-1-recoveryDay) % actrecoveryDay == 0 && day >= recoveryDay) {
             totalCases = totalCases - prevCatch;
-            totalRecovered = prevCatch;
+            totalRecovered = totalRecovered + prevCatch;
+            prevCatch = newCases[recoveryDayCount];
+            recoveryDayCount++;
         }
         if ((day-1) % (contagiousDays-1) == 0 && day != 1) {
             fooCatch = fooCatch * r0;
             totalCases = totalCases + fooCatch;
-            pplNotHadCovid = pplNotHadCovid - totalCases;
+            if(totalCases > ppl-totalRecovered) {
+                totalCases = ppl - totalRecovered;
+                pplNotHadCovid = 0;
+            } else pplNotHadCovid = pplNotHadCovid - fooCatch;
+            newCases[contagiousDayCount] = fooCatch;
+            contagiousDayCount++;
         }
-        cout << day << " " << totalCases << " " << totalRecovered << " " << pplNotHadCovid << "\n";
+        if(totalCases < 0) totalCases = 0;
+        if(totalRecovered > ppl) totalRecovered = ppl;
+        cout << setw(5) << day << setw(20) << totalCases << setw(20) << totalRecovered << setw(35)<< totalRecovered+totalCases << setw(30) << pplNotHadCovid << endl;
         day++;
     }
+    cout << endl;
+    cout << "Days Until Fully Recovered Population: " << day << endl;
+    cout << "----------------------------------------------------------------------------------------------------------------\n";
 
     return 0;
 }
