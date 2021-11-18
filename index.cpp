3ee52296ca096c386d89cb4be0da05976fde6311
Joshua <63396516+theradongkid2@users.noreply.github.com>
2021-11-18 19:07:01 +1100

    a

--- index.cpp
+++ index.cpp
@@ -3,31 +3,32 @@ using namespace std;
 
 int ppl, c0, r0, contagiousDays, recoveryDay;
 
-int main(void) {
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+int main() {
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
 
-    cin >> ppl >> c0 >> r0 >> contagiousDays, recoveryDay;
+    cin >> ppl >> c0 >> r0 >> contagiousDays >> recoveryDay;
 
     int day = 1;
-    int pplNotHadCovid = ppl;
+    int pplNotHadCovid = ppl-c0;
     int totalCases = c0;
     int prevCatch = c0;
+    int fooCatch = c0;
     int totalRecovered = 0;
 
-    cout << "Day: | Active Total COVID Cases: | Total Recovered: | Population never had COVID: ";
+    cout << "Day: | Active Total COVID Cases: | Total Recovered: | Population never had COVID: \n";
 
     while (pplNotHadCovid > 0) {
-        if (day % recoveryDay == 0) {
+        if ((day-1) % recoveryDay == 0 && day != 1) {
             totalCases = totalCases - prevCatch;
             totalRecovered = prevCatch;
         }
-        if (day % contagiousDays == 0) {
-            totalCases = totalCases + (totalCases * r0);
-            prevCatch = totalCases;
+        if ((day-1) % (contagiousDays-1) == 0 && day != 1) {
+            fooCatch = fooCatch * r0;
+            totalCases = totalCases + fooCatch;
             pplNotHadCovid = pplNotHadCovid - totalCases;
         }
-        cout << day << totalCases << totalRecovered << pplNotHadCovid << "\n";
+        cout << day << " " << totalCases << " " << totalRecovered << " " << pplNotHadCovid << "\n";
         day++;
     }
 
